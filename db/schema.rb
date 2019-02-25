ab18071439e8cdfbffd0d10a6abeae3c9ef076e2
Dan Vernon <danvernon@gmail.com>
2019-02-25 16:51:22 +0000

    new schema containing both users and bookings tables

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,15 @@ ActiveRecord::Schema.define(version: 2019_02_25_162412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.text "description"
+    t.date "date"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -46,4 +55,5 @@ ActiveRecord::Schema.define(version: 2019_02_25_162412) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "bookings", "users"
 end
