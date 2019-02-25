7f921ba92b01c2b13ee7d2af42374cfa8f6ccef8
Kitty Mayo <kitty.m.mayo@gmail.com>
2019-02-25 16:23:55 +0000

    created controller methods for bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   validates :description, presence: true
   validates :date, :presence: true
+  # add validation about date being available- help (https://medium.com/lightthefuse/ruby-on-rails-date-validation-in-a-booking-and-disabling-dates-in-date-picker-3e5b4e9b4640)
   validates :user, presence: true
-
 
 end
