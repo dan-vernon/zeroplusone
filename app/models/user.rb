10570196d4413f54a50c52cf58d01726bd10fc1a
Dan Vernon <danvernon@gmail.com>
2019-02-26 17:36:33 +0000

    signup form partial created | new registration points to partial | complex devise methods re email validation, timeout etc removed for simplicity while testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :recoverable, :rememberable, :validatable, :lockable, :timeoutable, :trackable, :omniauthable
+
   has_many :bookings_as_hero, :class_name => 'Booking', :foreign_key => 'hero_id'
   has_many :bookings_as_zero, :class_name => 'Booking', :foreign_key => 'zero_id'
   has_many :reviews
   validates :first_name, :last_name, :location, presence: true
   validates :email, presence: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/, uniqueness: true
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :lockable, :timeoutable, :trackable, :omniauthable
+  devise :database_authenticatable, :registerable
 end
