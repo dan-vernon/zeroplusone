380ca0d36cecb8b69ffd9472d411dfb1b0d74f80
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-02-26 15:35:58 +0000

    reviews controller added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,27 @@
 class ReviewsController < ApplicationController
   def index
+    @reviews = Review.all
   end
 
   def new
+    @user = User.find(params([:user_id]))
+    @review = Review.new
   end
 
   def create
+    @user = User.find(params([:user_id]))
+    @review = Review.new(review_params)
+    @review.user = @user
+    if @review.save
+      redirect_to reviews_index_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :booking_id, :user_id)
   end
 end
