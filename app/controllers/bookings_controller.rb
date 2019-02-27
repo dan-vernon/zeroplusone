ae7df67bb7ed2266030e18ddb07382c736911f00
Kitty Mayo <kitty.m.mayo@gmail.com>
2019-02-27 13:47:17 +0000

    created html fot bookings index

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,11 @@
 class BookingsController < ApplicationController
   def index
-    @bookings = Booking.all
+    @bookings = Booking.where(zero: current_user.id)
   end
 
   def show
     @booking = Booking.find(params[:id])
-    @bookings = Booking.all
+    @bookings = Booking.where(zero: current_user.id)
   end
 
   def new
