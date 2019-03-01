31c72d561ed048f0fadbea1b7da8eeef6f6921c8
Kitty Mayo <kitty.m.mayo@gmail.com>
2019-03-01 11:20:09 +0000

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,7 +34,7 @@ Skill.create!(
 # seed users
 puts "Seeding users"
 
-10.times do
+20.times do
   User.create!(
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
@@ -51,32 +51,123 @@ end
 
   User.create!(
   [{first_name: 'Alex',
-  last_name:  'Elsden',
+  last_name:  'E',
   email: 'alex@alex.com',
   location: 'London',
   bio: 'I used to work as a building surveyor for a private building control company',
   hero: true,
-  dob: faker.date.between('1945-01-01', '2001-01-01'),
+  dob: Faker::Date.between('1945-01-01', '2001-01-01'),
   password: "password",
-  photo: open("https://loremflickr.com/600/800/person")
-  }
-  # {first_name: ,
-  # last_name: ,
-  # email: ,
-  # location: ,
-  # bio: ,
-  # hero: ,
-  # dob: ,
-  # password: "password",
-  # photo: open("https://loremflickr.com/600/800/person")
-  # },
+  photo: open("https://avatars1.githubusercontent.com/u/46600044?v=4")
+  },
+  {first_name: 'Dan',
+  last_name:  'V',
+  email: 'dan@dan.com',
+  location: 'London',
+  bio: "Camberwell resident, belligerent cyclist, font of Hoxton/Haggerston eating/drinking knowledge. Self-appointed winner of Best 'Tache Batch #222",
+  hero: true,
+  dob: Faker::Date.between('1945-01-01', '2001-01-01'),
+  password: "password",
+  photo: open("https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/i2d2bgvj9lfpbyoo7vs1.jpg")
+  },
+  {first_name: 'Clementine',
+  last_name:  'C',
+  email: 'Clementine@Clementine.com',
+  location: 'London',
+  bio: 'I enjoy tennis, rock climbing, painting.',
+  hero: true,
+  dob: Faker::Date.between('1945-01-01', '2001-01-01'),
+  password: "password",
+  photo: open("https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/pkoojatqokuphwpgmopa.jpg")
+  },
+  {first_name: 'Edoardo',
+  last_name:  'O',
+  email: 'Edoardo@Edoardo.com',
+  location: 'London',
+  bio: 'I  have worked as a chef here in London for several years',
+  hero: true,
+  dob: Faker::Date.between('1945-01-01', '2001-01-01'),
+  password: "password",
+  photo: open("https://avatars0.githubusercontent.com/u/45206889?v=4")
+  },
+  {first_name: 'Esther',
+  last_name:  'C',
+  email: 'esther@esther.com',
+  location: 'London',
+  bio: 'I enjoy cooking, doing yoga, watching movies and musicals',
+  hero: true,
+  dob: Faker::Date.between('1945-01-01', '2001-01-01'),
+  password: "password",
+  photo: open("https://avatars2.githubusercontent.com/u/40442483?v=4")
+  },
+  {first_name: 'Hugo',
+  last_name:  'M',
+  email: 'Hugo@hugo.com',
+  location: 'London',
+  bio: 'I Previously worked in graphic design & branding jobs.',
+  hero: true,
+  dob: Faker::Date.between('1945-01-01', '2001-01-01'),
+  password: "password",
+  photo: open("https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/m9ut43h0zzjhpgkk3o1e.jpg")
+  },
+  {first_name: 'Izzy',
+  last_name:  'W',
+  email: 'Izzy@izzy.com',
+  location: 'London',
+  bio: 'I like the Economist, niche museums, and hiking!',
+  hero: true,
+  dob: Faker::Date.between('1945-01-01', '2001-01-01'),
+  password: "password",
+  photo: open("https://avatars1.githubusercontent.com/u/40670298?v=4")
+  },
+  {first_name: 'Jack',
+  last_name:  'B',
+  email: 'jack@jack.com',
+  location: 'London',
+  bio: 'I am a freelance film director, visionary and storyteller.',
+  hero: true,
+  dob: Faker::Date.between('1945-01-01', '2001-01-01'),
+  password: "password",
+  photo: open("https://avatars1.githubusercontent.com/u/39552144?v=4")
+  },
+  {first_name: 'Janie',
+  last_name:  'A',
+  email: 'Janie@Janie.com',
+  location: 'London',
+  bio: ' am always interesting in learning new things',
+  hero: true,
+  dob: Faker::Date.between('1945-01-01', '2001-01-01'),
+  password: "password",
+  photo: open("https://avatars3.githubusercontent.com/u/43782388?v=4")
+  },
+  {first_name: 'Jonny',
+  last_name:  'G',
+  email: 'Jonny@Jonny.com',
+  location: 'London',
+  bio: "I don't know what exactly I'll do after graduation",
+  hero: true,
+  dob: Faker::Date.between('1945-01-01', '2001-01-01'),
+  password: "password",
+  photo: open("https://avatars2.githubusercontent.com/u/40580116?v=4")
+  },
+  {first_name: 'Maddie',
+  last_name:  'L',
+  email: 'maddie@maddie.com',
+  location: 'London',
+  bio: '
+I just finished my A-levels and am on a gap year.',
+  hero: true,
+  dob: Faker::Date.between('1945-01-01', '2001-01-01'),
+  password: "password",
+  photo: open("https://avatars1.githubusercontent.com/u/34004218?v=4")
+  },
 
 ])
 
 
 puts "seeding UserSkills"
 
-10.times do
+50.times do
   UserSkill.create!(
     user: User.heroes.sample,
     skill: Skill.all.sample)
@@ -84,7 +175,7 @@ end
 
 puts "Seeding bookings"
 
-10.times do
+20.times do
   Booking.create!(
     description: Faker::Superhero.name,
     date: Faker::Date.between(20.days.ago, Date.today),
