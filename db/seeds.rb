ca013deb7605f691a15d9dcef041fbb1c169f837
Kitty Mayo <kitty.m.mayo@gmail.com>
2019-03-01 10:55:19 +0000

    created seed for alex

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,19 +17,19 @@ Skill.create!(
     {skillset: 'charming mothers in law'},
     {skillset: 'schmoozing business associates'},
     {skillset: 'dad dancing'},
-    {skillset: 'getting hilariuosly drunk'},
+    {skillset: 'getting hilariously drunk'},
     {skillset: 'getting on with difficult relatives'},
-    {skillset: 'convincing your grandma that you will pass on the family name' },
-    {skillset: 'foreign language'},
+    {skillset: 'convincing your grandmother that you are going to pass on the family name' },
+    {skillset: 'speaking a foreign language'},
     {skillset: 'great party tricks'},
-    {skillset: 'making friends with animals'},
+    {skillset: 'making friends with all animals'},
     {skillset: 'wine tasting'},
     {skillset: 'sartorial elegance'},
     {skillset: 'break dancing'},
     {skillset: 'karaoke'},
     {skillset: 'owns a gilet'},
     {skillset: 'recognising bird calls'},
-    {skillset: 'good at regional accents'}])
+    {skillset: 'good at most regional accents'}])
 
 # seed users
 puts "Seeding users"
@@ -48,6 +48,31 @@ puts "Seeding users"
   )
   # puts "User created"
 end
+
+  User.create!(
+  [{first_name: 'Alex',
+  last_name:  'Elsden',
+  email: 'alex@alex.com',
+  location: 'London',
+  bio: 'I used to work as a building surveyor for a private building control company',
+  hero: true,
+  dob: faker.date.between('1945-01-01', '2001-01-01'),
+  password: "password",
+  photo: open("https://loremflickr.com/600/800/person")
+  }
+  # {first_name: ,
+  # last_name: ,
+  # email: ,
+  # location: ,
+  # bio: ,
+  # hero: ,
+  # dob: ,
+  # password: "password",
+  # photo: open("https://loremflickr.com/600/800/person")
+  # },
+
+])
+
 
 puts "seeding UserSkills"
 
